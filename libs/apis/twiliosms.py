036e91b3ac9d55db23c1f4f47d49052f09ffd387
Demetrius <meetri@gmail.com>
2018-09-05 21:29:01 -0700

    ...

--- libs/apis/twiliosms.py
+++ libs/apis/twiliosms.py
@@ -4,9 +4,9 @@ from twilio.rest import Client
 class TwilioSms(object):
 
     def __init__(self,sms_from=None,sid=None,token=None):
-        self.sms_from = urllib.parse.quote_plus(os.getenv("TWILIO_FROM",sms_from))
-        sid = urllib.parse.quote_plus(os.getenv("TWILIO_SID",sid))
-        token = urllib.parse.quote_plus(os.getenv("TWILIO_TOKEN",token))
+        self.sms_from = str(os.getenv("TWILIO_FROM",sms_from))
+        sid = os.getenv("TWILIO_SID",sid)
+        token = os.getenv("TWILIO_TOKEN",token)
         self.client = Client(sid, token)
 
 
