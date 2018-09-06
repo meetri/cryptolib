1030e7f5d48a1bb99989fbcf004219cc1d2095df
Demetrius <meetri@gmail.com>
2018-09-05 21:43:23 -0700

    ...

--- libs/apis/twiliosms.py
+++ libs/apis/twiliosms.py
@@ -8,6 +8,9 @@ class TwilioSms(object):
         sid = os.getenv("TWILIO_SID",sid)
         token = os.getenv("TWILIO_TOKEN",token)
         self.client = Client(sid, token)
+        print("twilio_from: '{}'".format(self.sms_from))
+        print("twilio_sid: '{}'".format(sid))
+        print("twilio_token: '{}'".format(token))
 
 
     def send( self,msg, number):
